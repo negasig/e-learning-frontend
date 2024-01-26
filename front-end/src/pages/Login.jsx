8a2f17b117da1b141d37103c8306cdc185facaae
Biniam Belay <nigertsewar1984@gmail.com>
2024-01-26 14:02:40 +0300

    add logo

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
 
+import React from 'react'
+import ih from '../images/OIP.jpg'
+import kh from '../images/e-learning.jpg'
+import { Button } from '@mui/material';
 
 
 export default function Login() {
   localStorage.setItem("name", "negasi");
-   console.log(localStorage.getItem("name"));
+  console.log(localStorage.getItem("name"));
   return (
     <>
-    <h1>E-Learning</h1>
-    <div className='login'>
-    <form>
-    <h3>login</h3>
-    <label>username</label>
-    <input type='text' name='uname'/>
-    <label>password</label>
-    <input type='password' name='pass'/><br/>
-    <input type='submit'/>
- </form>
+    <div className='logo'>
+    <img src={kh} alt='aghfjj'/>
     </div>
-     </>
+      
+      <div className='login'>
+        <h3>E-learning</h3>
+        <img src={ih} alt='igkdbc'/>
+        <div className='login'>
+        <form>
+          <input type='text' name="username" placeholder='Enter username' required/><br/>
+          <input type='password' name="password" placeholder='Enter password' required/><br/>
+          <Button type='submit' variant='contained'>Login</Button>
+        </form>
+        </div>
+
+      </div>
+    </>
   )
-  
+
 }
