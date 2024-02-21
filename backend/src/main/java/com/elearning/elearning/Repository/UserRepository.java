44fc134167d42914e177347179e3b0fc93e4e0a5
Biniam Belay <nigertsewar1984@gmail.com>
2024-02-21 15:14:12 +0300

    commit

--- backend/src/main/java/com/elearning/elearning/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elearning.elearning.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.elearning.elearning.models.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
--- front-end/src/pages/protectedRoute.jsx
+++ front-end/src/pages/protectedRoute.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Login from './Login';
 export default function Protectedroute() {
-    var islogedIn=true;
+    var islogedIn=localStorage.getItem("token");
   return islogedIn ? <Outlet />:<Login />
-  
 }
